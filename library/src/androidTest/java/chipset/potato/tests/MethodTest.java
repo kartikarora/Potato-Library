7654eeb85e5e91bacabc618c458872368240b4b6
chipset <chipset95@yahoo.co.in>
2015-06-20 17:34:14 +0530

    Release 0.1.3

--- library/src/androidTest/java/chipset/potato/tests/MethodTest.java
+++ library/src/androidTest/java/chipset/potato/tests/MethodTest.java
@@ -42,8 +42,6 @@ public class MethodTest extends AndroidTestCase {
         assertFalse(utils.isBluetoothOn(getContext()));
         assertNotNull(utils.openDirectory(getContext(), "abcd"));
         assertTrue(0 <= utils.getBatteryLevel(getContext()) && utils.getBatteryLevel(getContext()) <= 100);
-        assertFalse(utils.isWiFiEnabled(getContext()));
-        assertFalse(utils.isGPSEnabled(getContext()));
 
     }
 

c27536e20bc88cb985db2742226870f6548e8ebb
anuraagbaishya <baishya.anuraag1920@gmail.com>
2015-12-06 18:00:41 +0530

    Calendar Intent, Camera Intent, Music Player Intent

--- library/src/main/java/chipset/potato/intents/Intents.java
+++ library/src/main/java/chipset/potato/intents/Intents.java
@@ -3,6 +3,8 @@ package chipset.potato.intents;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.CalendarContract;
+import android.provider.MediaStore;
 
 /**
  * Developer: chipset
@@ -71,6 +73,119 @@ public class Intents {
                 .setData(Uri.parse("sms:"))
                 .putExtra("sms_body", message)
                 .putExtra("address", phoneNumber)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    /**
+     * Method to call Calendar to add event.
+     *
+     * @param context     Context of the activity from where the intent is called
+     * @param startTime   Start date and time for the event in milliseconds since epoch
+     * @param endTime     End date and time for the event in milliseconds since epoch
+     * @param title       Title of the event
+     * @param allDay      Set to true for all day events, else false
+     * @param description Description of the event
+     *
+     * Requires API level 14
+     */
+    public void calenderIntent(Context context, long startTime, long endTime, String title, Boolean allDay, String description) {
+        context.startActivity(new Intent(Intent.ACTION_EDIT)
+                .setType("vnd.android.cursor.item/event")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTime)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME,endTime)
+                .putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, allDay)
+                .putExtra(CalendarContract.Events.TITLE, title)
+                .putExtra(CalendarContract.Events.DESCRIPTION,description)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    /**
+     * Method to call Calendar to add event with location of the event.
+     *
+     * @param context     Context of the activity from where the intent is called
+     * @param startTime   Start date and time for the event in milliseconds since epoch
+     * @param endTime     End date and time for the event in milliseconds since epoch
+     * @param title       Title of the event
+     * @param allDay      Set to true for all day events, else false
+     * @param description Description of the event
+     * @param location    Location of the event
+     *
+     * Requires API level 14
+     */
+    public void calenderIntentWithLocation(Context context, long startTime, long endTime, String title, Boolean allDay, String description, String location) {
+        context.startActivity(new Intent(Intent.ACTION_EDIT)
+                .setType("vnd.android.cursor.item/event")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTime)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime)
+                .putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, allDay)
+                .putExtra(CalendarContract.Events.TITLE, title)
+                .putExtra(CalendarContract.Events.DESCRIPTION,description)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    /**
+     * Method to call Music Player
+     *
+     * @param context Context of the activity from where the intent is called
+     */
+    public void musicPlayerIntent(Context context){
+        context.startActivity(new Intent("android.intent.category.APP_MUSIC")
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    /**
+     * Method to call Music Player and play the file specified in fileLocation
+     *
+     * @param context      Context of the activity from where the intent is called
+     * @param fileLocation Location of the audio file
+     * Requires permission WRITE_EXTERNAL_STORAGE
+     */
+    public void musicPlayerIntentWithFile(Context context, Uri fileLocation){
+        context.startActivity(new Intent(Intent.CATEGORY_APP_MUSIC)
+                .setAction(Intent.ACTION_VIEW)
+                .setDataAndType(fileLocation, "audio/*")
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    /**
+     * Method to call Music Player
+     *
+     * @param context Context of the activity from where the intent is called
+     * This method is deprecated as of API level 15 and is used for backward compatibility to API level 8
+     * It is advisable to use musicPlayerIntent() method for API level 15 and above
+     */
+    @Deprecated
+    public void musicIntent(Context context){
+        context.startActivity(new Intent("android.intent.action.MUSIC_PLAYER")
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    /**
+     * Method to call Music Player and play the file specified in fileLocation
+     *
+     * @param context      Context of the activity from where the intent is called
+     * @param fileLocation Location of the audio file
+     *
+     * Requires permission WRITE_EXTERNAL_STORAGE
+     * This method is deprecated as of API level 15 and is used for backward compatibility to API level 8
+     * It is advisable to use musicPlayerIntentWithFile() method for API level 15 and above
+     */
+    @Deprecated
+    public void musicIntentWithFile(Context context, Uri fileLocation){
+        context.startActivity(new Intent(MediaStore.INTENT_ACTION_MUSIC_PLAYER)
+                .setAction(Intent.ACTION_VIEW)
+                .setDataAndType(fileLocation,"audio/*")
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    /**
+     * Method to start Camera
+     *
+     * @param context Context of the activity from where the intent is called
+     */
+    public void cameraIntent(Context context){
+        context.startActivity(new Intent("android.media.action.IMAGE_CAPTURE")
                 .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
     }
 }
